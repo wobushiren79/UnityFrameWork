887456ee91d38f6f41599903733315c0ac56ab84
Apple\AppleCoffee <734719526@qq.com>
2023-03-20 23:13:54 +0800

    GameData类添加

--- Scrpits/Component/Handler/GameDataHandler.cs
+++ Scrpits/Component/Handler/GameDataHandler.cs
@@ -37,19 +37,5 @@ public partial class GameDataHandler : BaseHandler<GameDataHandler, GameDataMana
         return arrayData.ToList();
     }
 
-    /// <summary>
-    /// 初始化数据
-    /// </summary>
-    public void InitData()
-    {
-        GameConfigBean gameConfig = manager.GetGameConfig();
-        //设置全屏
-        Screen.fullScreen = gameConfig.window == 1 ? true : false;
-        //环境参数初始化
-        VolumeHandler.Instance.InitData();
-        //设置FPS
-        FPSHandler.Instance.SetData(gameConfig.stateForFrames, gameConfig.frames);
-        //修改抗锯齿
-        CameraHandler.Instance.ChangeAntialiasing(gameConfig.GetAntialiasingMode(),gameConfig.antialiasingQualityLevel);
-    }
+
 }
