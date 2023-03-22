1268367964897ebc39fcbe6aa7280079011790a6
Apple\AppleCoffee <734719526@qq.com>
2023-03-22 14:55:14 +0800

    添加按钮的callback

---
+++ Scrpits/CallBack/View/IRadioButtonCallBack.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public interface IRadioButtonCallBack
+{
+
+    /// <summary>
+    /// 单选按钮点击
+    /// </summary>
+    /// <param name="view"></param>
+    /// <param name="isSelect"></param>
+    void RadioButtonSelected(RadioButtonView view, bool isSelect);
+}
