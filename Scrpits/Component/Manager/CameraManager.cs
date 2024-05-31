7cd54a97232cf41442f9726c8542cf5f04862af6
Apple\AppleCoffee <734719526@qq.com>
2024-05-31 14:47:57 +0800

    框架优化

---
+++ Scrpits/Component/Manager/CameraManager.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+public partial class CameraManager : BaseManager
+{
+    //主摄像头
+    protected Camera _mainCamera;
+    public Camera mainCamera
+    {
+        get
+        {
+            if (_mainCamera == null)
+            {
+                _mainCamera = Camera.main;
+            }
+            return _mainCamera;
+        }
+    }
+
+    //ui摄像头
+    protected Camera _uiCamera;
+    public Camera uiCamera
+    {
+        get
+        {
+            if (_uiCamera == null)
+            {
+                //_uiCamera = FindWithTag<Camera>(TagInfo.Tag_UICamera);
+                _uiCamera = Camera.main;
+            }
+            return _uiCamera;
+        }
+    }
+}
