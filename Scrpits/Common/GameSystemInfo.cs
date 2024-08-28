4c1481dcd80d00d5f8aec8807c6c344d2d42e1f0
Apple\AppleCoffee <734719526@qq.com>
2024-08-28 22:55:05 +0800

    框架

---
+++ Scrpits/Common/GameSystemInfo.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystemInfo
+{
+    //∆¡ƒª∑÷±Ê¬ …Ë÷√
+    public static List<string> ListScreenResolutionData = new List<string>
+        {
+            "640x480",
+            "800x480",
+            "800x600",
+            "960x540",
+            "1024x600",
+            "1024x768",
+            "1280x720",
+            "1280x800",
+            "1280x1024",
+            "1366x768",
+            "1400x1050",
+            "1440x900",
+            "1600x900",
+            "1600x1200",
+            "1680x1050",
+            "1920x1080",
+            "1920x1200",
+            "2048x1536",
+            "2560x1440",
+            "2560x1600",
+            "3840x2160"
+        };
+}
