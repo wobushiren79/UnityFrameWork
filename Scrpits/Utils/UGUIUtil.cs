6ec65e626e4d54644021977811481ffa503287f2
Apple\AppleCoffee <734719526@qq.com>
2025-04-23 22:51:58 +0800

    优化

--- Scrpits/Utils/UGUIUtil.cs
+++ Scrpits/Utils/UGUIUtil.cs
@@ -10,11 +10,52 @@ public class UGUIUtil
     /// <summary>
     /// 获取TF在指定UIroot下的坐标
     /// </summary>
-    public static Vector3 GetUIRootPos(Transform tfRoot, Transform targetTF)
+    public static Vector3 GetRootPos(Transform tfRoot, Transform targetTF)
     {
-        return tfRoot.InverseTransformPoint(targetTF.position);
+        return GetRootPos(tfRoot, targetTF.position);
     }
 
+    public static Vector3 GetRootPos(Transform tfRoot, Vector3 targetPosition)
+    {
+        return tfRoot.InverseTransformPoint(targetPosition);
+    }
+
+    /// <summary>
+    /// 未验证
+    /// </summary>
+    public static Vector2 GetRootPosForUI(RectTransform child, RectTransform newParent, Camera cameraParent)
+    {
+        // 获取子控件的屏幕坐标
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(cameraParent, child.position);
+
+        // 转换为目标父控件的局部坐标
+        Vector2 localPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            newParent,
+            screenPos,
+            cameraParent,
+            out localPos
+        );
+
+        return localPos;
+    }
+
+    public static Vector2 GetRootPosForUI(RectTransform child, RectTransform originalParent, RectTransform newParent)
+    {
+        return GetRootPosForUI(child.anchoredPosition,  originalParent,  newParent);
+    }
+
+    public static Vector2 GetRootPosForUI(Vector2 childPosition, RectTransform originalParent, RectTransform newParent)
+    {
+        // 1. 获取子控件在世界空间中的位置
+        Vector3 worldPosition = originalParent.TransformPoint(childPosition);
+
+        // 2. 将世界坐标转换为新父节点的局部空间
+        Vector3 localPosition = newParent.InverseTransformPoint(worldPosition);
+
+        return localPosition;
+    }
+    
     /// <summary>
     /// 是否点击到了UI
     /// </summary>
