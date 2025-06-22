56e663f80710f28bc0b6c43b5438205179c70342
Apple\AppleCoffee <734719526@qq.com>
2025-06-22 18:34:36 +0800

    添加一些例子

--- Scrpits/Component/Handler/CameraHandler.cs
+++ Scrpits/Component/Handler/CameraHandler.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Cinemachine;
 using UnityEngine;
 
 public partial class CameraHandler : BaseHandler<CameraHandler, CameraManager>
@@ -15,5 +16,24 @@ public partial class CameraHandler : BaseHandler<CameraHandler, CameraManager>
             //target.eulerAngles = CameraHandler.Instance.manager.mainCamera.transform.eulerAngles;
             target.eulerAngles = Vector3.zero;
         }
+    }
+
+    /// <summary>
+    /// 获取跟随物体距离
+    /// </summary>
+    /// <param name="virtualCamera"></param>
+    /// <returns></returns>
+    public float GetDistanceFollow(CinemachineVirtualCamera virtualCamera)
+    {
+        if (virtualCamera == null)
+        {
+            LogUtil.LogError($" 获取跟随物体距离失败 virtualCamera为null");
+            return 0;
+        }
+        // 获取 Transposer 组件
+        CinemachineTransposer transposerBase = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        Vector3 followOffset = transposerBase.m_FollowOffset;
+        float disFollow = Vector3.Distance(followOffset, Vector3.zero);
+        return disFollow;
     }
 }
