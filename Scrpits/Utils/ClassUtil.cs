d86b309b0df191b6b2ba70a690584addc114702a
Apple\AppleCoffee <734719526@qq.com>
2025-10-09 22:13:41 +0800

    深度复制

--- Scrpits/Utils/ClassUtil.cs
+++ Scrpits/Utils/ClassUtil.cs
@@ -10,24 +10,47 @@ using System.Runtime.Serialization;
 public class ClassUtil : MonoBehaviour
 {
     /// <summary>
-    /// 打包后可能有点问题
+    /// 深度复制 注意：使用这个方法需要给类添加一个默认的构造函数
     /// </summary>
     public static T DeepCopy<T>(T obj)
     {
         if (obj == null) return default(T);
 
+        // 如果是Unity对象，使用JsonUtility
+        if (obj is UnityEngine.Object)
+        {
+            return DeepCopyUnityObject(obj);
+        }
+
+        return DeepCopySystemObject(obj);
+    }
+
+    /// <summary>
+    /// 深度复制 注意：使用这个方法需要给类添加一个默认的构造函数
+    /// </summary>
+    private static T DeepCopyUnityObject<T>(T obj)
+    {
+        string json = JsonUtility.ToJson(obj);
+        T copy = (T)Activator.CreateInstance(obj.GetType());
+        JsonUtility.FromJsonOverwrite(json, copy);
+        return copy;
+    }
+    
+    /// <summary>
+    /// 深度复制 注意：使用这个方法需要给类添加一个默认的构造函数
+    /// </summary>
+    private static T DeepCopySystemObject<T>(T obj)
+    {
+        if (obj == null) return default(T);
+
         Type type = obj.GetType();
 
-        // 如果对象是值类型或字符串，则直接返回
         if (type.IsValueType || type == typeof(string))
-        {
             return obj;
-        }
-        // 如果对象是数组，则创建一个新数组，并复制每个元素
-        else if (type.IsArray)
+
+        if (type.IsArray)
         {
-            Type elementType = Type.GetType(
-                type.FullName.Replace("[]", string.Empty));
+            Type elementType = type.GetElementType();
             var array = obj as Array;
             Array copied = Array.CreateInstance(elementType, array.Length);
             for (int i = 0; i < array.Length; i++)
@@ -36,68 +59,104 @@ public class ClassUtil : MonoBehaviour
             }
             return (T)(object)copied;
         }
-        // 如果对象是类，则递归复制每个属性
-        else if (type.IsClass)
+
+        if (type.IsClass)
         {
-            object copiedObj = Activator.CreateInstance(obj.GetType());
-            PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            foreach (PropertyInfo property in properties)
-            {
-                if (!property.CanWrite || !property.CanRead) continue;
-                object propertyValue = property.GetValue(obj, null);
-                if (propertyValue == null) continue;
-                property.SetValue(copiedObj, DeepCopy(propertyValue), null);
-            }
-            return (T)copiedObj;
+            object copy = Activator.CreateInstance(type);
+            CopyFields(obj, copy, type);
+            CopyProperties(obj, copy, type);
+            return (T)copy;
         }
-        else
+
+        return default(T);
+    }
+    
+    private static void CopyFields(object source, object target, Type type)
+    {
+        FieldInfo[] fields = type.GetFields(
+            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            
+        foreach (FieldInfo field in fields)
         {
-            throw new ArgumentException("Unsupported type");
+            // 跳过只读字段和常量
+            if (field.IsLiteral || field.IsInitOnly) continue;
+            
+            object value = field.GetValue(source);
+            if (value != null)
+            {
+                field.SetValue(target, DeepCopy(value));
+            }
+            else
+            {
+                field.SetValue(target, null);
+            }
         }
     }
-
-    public static T DeepCopyBySerialize<T>(T obj)
+    
+    private static void CopyProperties(object source, object target, Type type)
     {
+        PropertyInfo[] properties = type.GetProperties(
+            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            
+        foreach (PropertyInfo property in properties)
+        {
+            if (!property.CanRead || !property.CanWrite) continue;
+            if (property.GetIndexParameters().Length > 0) continue; // 跳过索引器
+            
+            object value = property.GetValue(source);
+            if (value != null)
+            {
+                property.SetValue(target, DeepCopy(value));
+            }
+            else
+            {
+                property.SetValue(target, null);
+            }
+        }
+    }
+    
+    public static T CopyComponent<T>(T original, GameObject destination) where T : Component
+    {
+        Type type = original.GetType();
+        Component copy = destination.AddComponent(type);
+
+        FieldInfo[] fields = type.GetFields(
+            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (FieldInfo field in fields)
+        {
+            if (field.IsLiteral || field.IsInitOnly) continue;
+            field.SetValue(copy, field.GetValue(original));
+        }
+
+        return copy as T;
+    }
+    
+    public static T CopyScriptableObject<T>(T original) where T : ScriptableObject
+    {
+        T instance = ScriptableObject.CreateInstance<T>();
+        
+        string json = JsonUtility.ToJson(original);
+        JsonUtility.FromJsonOverwrite(json, instance);
+        
+        return instance;
+    }
+
+    public static T DeepCopyBinary<T>(T obj)
+    {
+        if (!typeof(T).IsSerializable)
+        {
+            throw new ArgumentException("The type must be serializable.", nameof(obj));
+        }
+
         if (obj == null) return default(T);
 
-        BinaryFormatter formatter = new BinaryFormatter();
-        using (MemoryStream stream = new MemoryStream())
+        using (var stream = new MemoryStream())
         {
+            BinaryFormatter formatter = new BinaryFormatter();
             formatter.Serialize(stream, obj);
             stream.Seek(0, SeekOrigin.Begin);
             return (T)formatter.Deserialize(stream);
         }
     }
-
-
-    public static T DeepCopyByXml<T>(T obj)
-    {
-        object retval;
-        using (MemoryStream ms = new MemoryStream())
-        {
-            XmlSerializer xml = new XmlSerializer(typeof(T));
-            xml.Serialize(ms, obj);
-            ms.Seek(0, SeekOrigin.Begin);
-            retval = xml.Deserialize(ms);
-            ms.Close();
-        }
-        return (T)retval;
-    }
-
-    public static T DeepCopyByBin<T>(T obj)
-    {
-        object retval;
-        using (MemoryStream ms = new MemoryStream())
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            //序列化成流
-            bf.Serialize(ms, obj);
-            ms.Seek(0, SeekOrigin.Begin);
-            //反序列化成对象
-            retval = bf.Deserialize(ms);
-            ms.Close();
-        }
-        return (T)retval;
-    }
-
 }
