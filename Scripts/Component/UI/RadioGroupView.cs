6c69df5fe8d1fe0b25c1aef8b19748248a29ea09
wobushiren79 <734719526@qq.com>
2026-06-05 18:31:19 +0800

    RadioGroupView 框架完善

--- Scripts/Component/UI/RadioGroupView.cs
+++ Scripts/Component/UI/RadioGroupView.cs
@@ -1,28 +1,63 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 
+/// <summary>
+/// 单选按钮组
+/// 管理一组 RadioButtonView，保证同一时刻只有一个按钮处于选中状态，并向外派发选中/取消回调
+/// </summary>
 public class RadioGroupView : BaseMonoBehaviour, IRadioButtonCallBack
 {
-    //是否能取消选择想
+    #region 字段
+
+    /// <summary>
+    /// 是否允许取消选择(为 true 时点击已选中按钮可取消选中，组内可同时无选中项)
+    /// </summary>
     public bool isCancelSelect = false;
-    //按钮列表
+
+    /// <summary>
+    /// 组内的单选按钮列表
+    /// </summary>
     public List<RadioButtonView> listButton;
+
+    /// <summary>
+    /// 按钮组选中状态变化的外部回调
+    /// </summary>
     private IRadioGroupCallBack mRGCallBack;
 
+    #endregion
 
+    #region 生命周期
+
+    /// <summary>
+    /// 初始化时为列表内每个按钮注册本组为回调对象
+    /// </summary>
     private void Start()
     {
         if (listButton.IsNull())
         {
             return;
         }
-        foreach (RadioButtonView itemRB in listButton)
+        for (int i = 0; i < listButton.Count; i++)
         {
+            RadioButtonView itemRB = listButton[i];
+            if (itemRB == null)
+            {
+                LogUtil.LogError($"RadioGroupView[{gameObject.name}] Start: listButton 第 {i} 个元素为空，请检查预制体上 RadioGroupView 组件的 List Button 是否存在未赋值的空槽位");
+                continue;
+            }
             itemRB.SetCallBack(this);
         }
     }
 
+    #endregion
+
+    #region 公有方法
+
+    /// <summary>
+    /// 向按钮组动态添加一个单选按钮，并为其注册本组为回调对象
+    /// </summary>
+    /// <param name="targetRB">要加入的单选按钮</param>
     public void AddRadioButton(RadioButtonView targetRB)
     {
         if (listButton == null)
@@ -33,6 +68,11 @@ public class RadioGroupView : BaseMonoBehaviour, IRadioButtonCallBack
         targetRB.SetCallBack(this);
     }
 
+    /// <summary>
+    /// 设置选中指定下标的按钮，其余按钮置为未选中
+    /// </summary>
+    /// <param name="position">要选中的按钮下标</param>
+    /// <param name="isCallBack">选中后是否触发外部选中回调</param>
     public void SetPosition(int position, bool isCallBack)
     {
         if (listButton == null)
@@ -42,6 +82,11 @@ public class RadioGroupView : BaseMonoBehaviour, IRadioButtonCallBack
         for (int i = 0; i < listButton.Count; i++)
         {
             RadioButtonView itemRB = listButton[i];
+            if (itemRB == null)
+            {
+                LogUtil.LogError($"RadioGroupView[{gameObject.name}] SetPosition: listButton 第 {i} 个元素为空，请检查预制体上的 List Button 空槽位");
+                continue;
+            }
             if (i == position)
             {
                 itemRB.ChangeStates(true);
@@ -59,7 +104,7 @@ public class RadioGroupView : BaseMonoBehaviour, IRadioButtonCallBack
     }
 
     /// <summary>
-    /// 自动找到rb
+    /// 自动从子物体收集所有 RadioButtonView 填充按钮列表，并为其注册本组为回调对象
     /// </summary>
     public void InitRadioButton()
     {
@@ -71,22 +116,37 @@ public class RadioGroupView : BaseMonoBehaviour, IRadioButtonCallBack
             listButton = TypeConversionUtil.ArrayToList(rbList);
         if (listButton != null)
         {
-            foreach (RadioButtonView itemRB in listButton)
+            for (int i = 0; i < listButton.Count; i++)
             {
+                RadioButtonView itemRB = listButton[i];
+                if (itemRB == null)
+                {
+                    LogUtil.LogError($"RadioGroupView[{gameObject.name}] InitRadioButton: 收集到的第 {i} 个 RadioButtonView 为空");
+                    continue;
+                }
                 itemRB.SetCallBack(this);
             }
         }
     }
 
     /// <summary>
-    /// 设置回调
+    /// 设置按钮组选中状态变化的外部回调
     /// </summary>
-    /// <param name="callback"></param>
+    /// <param name="callback">外部回调对象</param>
     public void SetCallBack(IRadioGroupCallBack callback)
     {
         this.mRGCallBack = callback;
     }
 
+    #endregion
+
+    #region IRadioButtonCallBack 回调
+
+    /// <summary>
+    /// 单个按钮被点击时的回调：将被点击按钮置为选中、其余置为未选中，并向外派发选中/取消回调
+    /// </summary>
+    /// <param name="view">被点击的按钮</param>
+    /// <param name="isSelect">被点击按钮的目标选中状态</param>
     public void RadioButtonSelected(RadioButtonView view, bool isSelect)
     {
         if (listButton.IsNull())
@@ -96,6 +156,11 @@ public class RadioGroupView : BaseMonoBehaviour, IRadioButtonCallBack
         for (int i = 0; i < listButton.Count; i++)
         {
             RadioButtonView itemRB = listButton[i];
+            if (itemRB == null)
+            {
+                LogUtil.LogError($"RadioGroupView[{gameObject.name}] RadioButtonSelected: listButton 第 {i} 个元素为空，请检查预制体上的 List Button 空槽位");
+                continue;
+            }
             if (itemRB.Equals(view))
             {
                 if (!isCancelSelect)
@@ -113,4 +178,6 @@ public class RadioGroupView : BaseMonoBehaviour, IRadioButtonCallBack
             }
         }
     }
+
+    #endregion
 }
